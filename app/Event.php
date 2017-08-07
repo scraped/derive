f8fe295b595239b45237aa072d5944c6d4bf2bd7
Ken Price <kenaprice@gmail.com>
2017-08-07 19:06:47 -0400

    Return location and event info

--- app/Event.php
+++ app/Event.php
@@ -10,6 +10,61 @@ class Event extends Model
 {
     public static function search($lat, $lng, $dist=15)
     {
+        $locations = static::getLocations($lat, $lng, $dist);
+
+        $locationIds = collect($locations)->map(function($item) {
+            return $item->id;
+        })->implode(',');
+
+        return static::getEvents($locationIds);
+    }
+
+    private static function getEvents($locationIds)
+    {
+        $client = new GuzzleHttp\Client();
+
+        $eventsFields = [
+            "id",
+            "type",
+            "name",
+            "cover.fields(id,source)",
+            "picture.type(large)",
+            "description",
+            "start_time",
+            "end_time",
+            "category",
+            "attending_count",
+            "declined_count",
+            "maybe_count",
+            "noreply_count"
+        ];
+        $locationFields = [
+            "id",
+            "name",
+            "about",
+            "emails",
+            "cover.fields(id,source)",
+            "picture.type(large)",
+            "category",
+            "category_list.fields(name)",
+            "location",
+            "events.fields(" . implode($eventsFields, ',') . ")"
+        ];
+
+        $res = $client->request('GET', 'https://graph.facebook.com/v2.10/', [
+            'query' => [
+                'access_token' => env('GRAPH_API_CLIENT_ID') . '|' . env('GRAPH_API_SECRET'),
+                'ids' => $locationIds,
+                'fields' => implode($locationFields, ',')
+            ]
+        ]);
+
+        $resBody = json_decode($res->getBody()->getContents());
+        return $resBody;
+    }
+
+    private static function getLocations($lat, $lng, $dist)
+    {
         $locations = Cache::remember("$lat,$lng,$dist", 1, function() use ($lat, $lng, $dist) {
             $client = new GuzzleHttp\Client();
             $locations = [];
@@ -34,10 +89,6 @@ class Event extends Model
             return $locations;
         });
 
-        $locationIds = collect($locations)->map(function($item) {
-            return $item->id;
-        })->implode(',');
-
-        return $locationIds;
+        return $locations;
     }
 }
