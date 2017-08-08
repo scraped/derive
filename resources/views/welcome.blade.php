8144cebef4a24520c36c7e624cba20132cdec35c
Ken Price <kenaprice@gmail.com>
2017-08-08 16:49:27 -0400

    Include FB js API

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,25 @@
         </script>
     </head>
     <body>
+        <script>
+            window.fbAsyncInit = function() {
+                FB.init({
+                    appId      : '1915610145354409',
+                    cookie     : true,
+                    xfbml      : true,
+                    version    : 'v2.8'
+                });
+                FB.AppEvents.logPageView();
+            };
+
+            (function(d, s, id){
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {return;}
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/en_US/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
         <div id="app">
             <div class="content mt-lg mc">
                 <div class="title m-b-md">
@@ -33,5 +52,13 @@
             </div>
         </div>
         <script src="/js/app.js"></script>
+        <script>
+            $(document).ready(function() {
+                FB.getLoginStatus(function(response) {
+                    console.log(response);
+                    statusChangeCallback(response);
+                });
+            });
+        </script>
     </body>
 </html>
