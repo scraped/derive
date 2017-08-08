cb27d9d26bd11b1704d44a216ef58c9834bc37d8
Ken Price <kenaprice@gmail.com>
2017-08-07 23:45:24 -0400

    Search from Vue component (not fully implemented)

--- resources/assets/js/components/events-search.vue
+++ resources/assets/js/components/events-search.vue
@@ -19,12 +19,19 @@
                 </div>
             </div>
         </div>
+        <div class="row" v-if="geoError">
+            <div class="col-md-8 col-md-offset-2">
+                TEST
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import * as VueGoogleMaps from 'vue2-google-maps';
     import Vue from 'vue';
+
+    const moment = require('moment');
 
     Vue.use(VueGoogleMaps, {
         load: {
@@ -34,27 +41,57 @@
     });
 
     export default {
+        components: { VueGoogleAutocomplete },
+
         data() {
             var center = {lat: 10.0, lng: 10.0};
             var marker = {lat: 10.0, lng: 10.0};
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition((position) => {
-                    center.lat = position.coords.latitude;
-                    center.lng = position.coords.longitude;
-                    marker.lat = position.coords.latitude;
-                    marker.lng = position.coords.longitude;
-                });
-            }
 
             return {
                 center,
-                marker
+                marker,
+                enabled: false,
+                geoError: false
             };
+        },
+
+        mounted() {
+            if (!navigator.geolocation) {
+                this.enabled = false;
+                return;
+            }
+
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    this.$set(this.center, 'lat', position.coords.latitude);
+                    this.$set(this.center, 'lng', position.coords.longitude);
+                    this.$set(this.marker, 'lat', position.coords.latitude);
+                    this.$set(this.marker, 'lng', position.coords.longitude);
+                    this.enabled = true;
+                },
+                (error) => {
+                    console.log(error);
+                    this.geoError = true;
+                },
+                {
+                    timeout: 10000
+                });
         },
 
         methods: {
             search() {
+                const {lat, lng} = this.marker;
 
+                axios.get('/events/search', {
+                    params: {
+                        lat,
+                        lng,
+                        date:  moment().format()
+                    }
+                })
+                    .then((response) => {
+                        console.log(response);
+                    });
             }
         }
     }
