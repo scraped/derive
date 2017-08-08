833f0cadce8c15ee677b735285e8a6c9f080ca78
Ken Price <kenaprice@gmail.com>
2017-08-07 22:20:56 -0400

    Basic google maps integration

--- resources/assets/js/components/events-search.vue
+++ resources/assets/js/components/events-search.vue
@@ -2,11 +2,19 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
-
-                    <div class="panel-body">
-                        I'm an example component!
+                <gmap-map
+                    :center="center"
+                    :zoom="7"
+                    map-type-id="terrain"
+                    style="width: 500px; height: 300px; margin: 0 auto;">
+                    <gmap-marker :position="marker">
+                    </gmap-marker>
+                </gmap-map>
+            </div>
+            <div class="col-md-8 col-md-offset-2" style="padding-top:20px">
+                <div class="form-inline">
+                    <div class="form-group">
+                        <input type="button" class="btn btn-primary form-control" value="Go" v-on:click="search">
                     </div>
                 </div>
             </div>
@@ -15,13 +23,39 @@
 </template>
 
 <script>
+    import * as VueGoogleMaps from 'vue2-google-maps';
+    import Vue from 'vue';
+
+    Vue.use(VueGoogleMaps, {
+        load: {
+            key: 'AIzaSyDBBuh9aUBOJuO0yGB0EDaSz5HmD8lnXKY',
+            libraries: 'places'
+        }
+    });
+
     export default {
         data() {
-            return {};
+            var center = {lat: 10.0, lng: 10.0};
+            var marker = {lat: 10.0, lng: 10.0};
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition((position) => {
+                    center.lat = position.coords.latitude;
+                    center.lng = position.coords.longitude;
+                    marker.lat = position.coords.latitude;
+                    marker.lng = position.coords.longitude;
+                });
+            }
+
+            return {
+                center,
+                marker
+            };
         },
 
-        mounted() {
-            console.log('Component mounted.');
+        methods: {
+            search() {
+
+            }
         }
     }
 </script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Laravel</title>
 
@@ -11,6 +12,7 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <style>
             html, body {
                 background-color: #fff;
